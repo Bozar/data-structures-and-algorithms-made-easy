4e684e86afb2f06aa4f963da5d693f16b19d4eb8
bozar <5583771+Bozar@users.noreply.github.com>
2026-01-17 22:25:36 +0800

    Add a macro: PRINT_LOG().

--- include/debug.h
+++ include/debug.h
@@ -6,22 +6,28 @@
 
 //#define NDEBUG
 #include <assert.h>
-// DEBUG_MACRO {{{1
+// SWITCH_DEBUG_MODE {{{1
 
 #if ! defined(NDEBUG)
 
 #define DEBUG_LLT 0
 
 #endif
-
+// PRINT_MESSAGE {{{1
 
 #define PRINT_ERR(...) do { \
 	fprintf(stderr, "%s:%d: %s: ", __FILE__, __LINE__, __func__); \
 	fprintf(stderr, __VA_ARGS__); \
 	fprintf(stderr, "\n"); \
 } while(0)
-// PRAGMA_GCC {{{1
 
+
+#define PRINT_LOG(...) do { \
+	fprintf(stdout, "%s:%d: %s: ", __FILE__, __LINE__, __func__); \
+	fprintf(stdout, __VA_ARGS__); \
+	fprintf(stdout, "\n"); \
+} while(0)
+// PRAGMA_GCC {{{1
 // https://www.fluentcpp.com/2019/08/30/how-to-disable-a-warning-in-cpp/
 
 #define DO_PRAGMA(X) \
