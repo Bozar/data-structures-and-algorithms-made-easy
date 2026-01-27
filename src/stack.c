3e3e501d1d37ccc17d79609db671e3bde9131214
bozar <5583771+Bozar@users.noreply.github.com>
2026-01-27 16:03:24 +0800

    Add linked list stack functions: push(), pop(), peek().

--- src/stack.c
+++ src/stack.c
@@ -16,6 +16,7 @@ NO_WARNING(-Wunused-function)
 
 // new(), is_empty(), size(), free()
 static bool zz_stk_lnew(struct stk_lstack ** stack);
+static bool zz_stk_loperate(void);
 
 
 static bool
@@ -38,12 +39,87 @@ zz_stk_lnew(struct stk_lstack ** stack) {
 	PRINT_LOG("Passed.");
 	return true;
 }
+
+
+static bool
+zz_stk_loperate(void) {
+	struct stk_lstack * stack = NULL;
+	bool is_ok = false;
+	if (! stk_lnew(&stack)) {
+		PRINT_ERR("Failed: stk_lnew()");
+		goto CLEAN_UP;
+	}
+
+	int data = -1;
+	for (int i = 0; i < 9; i++) {
+		if (! stk_lpush(stack, i)) {
+			PRINT_ERR("Failed: stk_lpush()");
+			goto CLEAN_UP;
+		} else if (stk_lis_empty(stack)) {
+			PRINT_ERR("Failed: stk_lis_empty()");
+			goto CLEAN_UP;
+		} else if (stk_lsize(stack) != i+1) {
+			PRINT_ERR("Failed: stk_lsize()");
+			goto CLEAN_UP;
+		}
+		data = -1;
+		if ((! stk_lpeek(stack, &data)) || (data != i)) {
+			PRINT_ERR("Failed: stk_lpeek()");
+			goto CLEAN_UP;
+		}
+	}
+
+	for (int i = 8; i > 0; i--) {
+		data = -1;
+		if ((! stk_lpop(stack, &data)) || (data != i)) {
+			PRINT_ERR("Failed: stk_lpop()");
+			goto CLEAN_UP;
+		} else if (stk_lis_empty(stack)) {
+			PRINT_ERR("Failed: stk_lis_empty()");
+			goto CLEAN_UP;
+		} else if (stk_lsize(stack) != i) {
+			PRINT_ERR("Failed: stk_lsize()");
+			goto CLEAN_UP;
+		}
+		data = -1;
+		if ((! stk_lpeek(stack, &data)) || (data != i-1)) {
+			PRINT_ERR("Failed: stk_lpeek()");
+			goto CLEAN_UP;
+		}
+	}
+	data = -1;
+	if ((! stk_lpop(stack, &data)) || (data != 0)) {
+		PRINT_ERR("Failed: stk_lpop()");
+		goto CLEAN_UP;
+	}
+
+	if (stk_lpop(stack, &data)) {
+		PRINT_ERR("Failed: stk_lpop()");
+		goto CLEAN_UP;
+	} else if (! stk_lis_empty(stack)) {
+		PRINT_ERR("Failed: stk_lis_empty()");
+		goto CLEAN_UP;
+	} else if (stk_lsize(stack) != 0) {
+		PRINT_ERR("Failed: stk_lsize()");
+		goto CLEAN_UP;
+	} else if (stk_lpeek(stack, &data)) {
+		PRINT_ERR("Failed: stk_lpeek()");
+		goto CLEAN_UP;
+	}
+
+	is_ok = true;
+CLEAN_UP:
+	stk_lfree(&stack);
+	if (is_ok) {
+		PRINT_LOG("Passed.");
+	}
+	return is_ok;
+}
 // STK_TEST {{{2
 
 bool
 stk_test(void) {
-	struct stk_lstack * stack = NULL;
-	return zz_stk_lnew(&stack);
+	return zz_stk_loperate();
 }
 
 NO_WARNING_END
@@ -93,4 +169,49 @@ stk_lfree(struct stk_lstack ** stack) {
 	}
 	free(*stack);
 	*stack = NULL;
+}
+
+
+bool
+stk_lpush(struct stk_lstack * stack, int data) {
+	struct llt_snode * node = NULL;
+	if (stack == NULL) {
+		return false;
+	} else if (! llt_snew(&node)) {
+		return false;
+	}
+
+	node->data = data;
+	node->next = stack->top;
+	stack->top = node;
+	stack->size += 1;
+	return true;
+}
+
+
+bool
+stk_lpop(struct stk_lstack * stack, int * data) {
+	if (stack == NULL) {
+		return false;
+	} else if (stack->top == NULL) {
+		return false;
+	}
+
+	*data = (stack->top)->data;
+	stack->top = (stack->top)->next;
+	stack->size -= 1;
+	return true;
+}
+
+
+bool
+stk_lpeek(struct stk_lstack * stack, int * data) {
+	if (stack == NULL) {
+		return false;
+	} else if (stack->top == NULL) {
+		return false;
+	}
+
+	*data = (stack->top)->data;
+	return true;
 }
